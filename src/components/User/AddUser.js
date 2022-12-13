e6fdbd0f05364b79ebc1a9150b44663d84a6ba63
wlsdpcks <80802907+wlsdpcks@users.noreply.github.com>
2022-12-13 20:16:35 +0900

    검증추가 및 로직 재설정하기

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -9,7 +9,16 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      // 더 안전하기 하기 위해 +를 붙여주기(입력된 나이값을 숫자형으로 바꿔주기)
+      return;
+    }
     console.log(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
   };
   const usernameChangeHandler = (event) => {
     setEnteredUsername(event.target.value);
@@ -22,9 +31,19 @@ const AddUser = (props) => {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler} />
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
