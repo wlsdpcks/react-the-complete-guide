81693843d6697906663d1b962bf1a7c023eca8f9
wlsdpcks <80802907+wlsdpcks@users.noreply.github.com>
2022-12-23 15:27:09 +0900

    113, 114. 리액트 포털 소개 및 작업하기

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Wrapper from "../../Helper/Wrapper";
+import Wrapper from "../../Helpers/Wrapper";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
